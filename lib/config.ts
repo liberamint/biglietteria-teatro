236ebb560ce368fceb9387037b7190114cd8c08f
Adolfo Squitieri <liberamint@gmail.com>
2026-04-08 18:39:37 +0200

    fix datetime spettacoli

--- lib/config.ts
+++ lib/config.ts
@@ -1,10 +1,40 @@
 export const TOTAL_POSTI = 188;
 
 export const SHOWS = [
-  { name: 'Lisistrata', slug: 'lisistrata', datetime: '13 Giugno ore 21' prefix: 'S1' },
-  { name: 'Pillole di Teatro', slug: 'pillole-di-teatro', datetime: '30 Maggio ore 21' prefix: 'S2' },
-  { name: 'Il Teatro Comico', slug: 'il-teatro-comico', datetime: '30 Maggio ore 18' prefix: 'S3' },
-  { name: 'La Famiglia Addams', slug: 'la-famiglia-addams', datetime: 14 Giugno ore 21'' prefix: 'S4' },
-  { name: 'Orario di Visita', slug: 'orario-di-visita', datetime: '24Maggio ore 21' prefix: 'S5' },
-  { name: 'Padre Incerto Madre Pure', slug: 'padre-incerto-madre-pure', datetime: '23 Maggio ore 21' prefix: 'S6' },
+  {
+    name: 'Lisistrata',
+    slug: 'lisistrata',
+    datetime: '2026-06-13T21:00:00',
+    prefix: 'S1',
+  },
+  {
+    name: 'Pillole di Teatro',
+    slug: 'pillole-di-teatro',
+    datetime: '2026-05-30T21:00:00',
+    prefix: 'S2',
+  },
+  {
+    name: 'Il Teatro Comico',
+    slug: 'il-teatro-comico',
+    datetime: '2026-05-30T18:00:00',
+    prefix: 'S3',
+  },
+  {
+    name: 'La Famiglia Addams',
+    slug: 'la-famiglia-addams',
+    datetime: '2026-06-14T21:00:00',
+    prefix: 'S4',
+  },
+  {
+    name: 'Orario di Visita',
+    slug: 'orario-di-visita',
+    datetime: '2026-05-24T21:00:00',
+    prefix: 'S5',
+  },
+  {
+    name: 'Padre Incerto Madre Pure',
+    slug: 'padre-incerto-madre-pure',
+    datetime: '2026-05-23T21:00:00',
+    prefix: 'S6',
+  },
 ] as const;
